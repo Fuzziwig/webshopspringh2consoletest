916b3ccfba47778f4f20f4d63d420e6ec0c57168
FrederikKea <fred1952@stud.kea.dk>
2020-10-30 10:55:23 +0100

    clone from first repo webshopSpring

---
+++ src/test/java/com/example/webshopspringh2console/WebshopSpringH2ConsoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webshopspringh2console;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopSpringH2ConsoleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
